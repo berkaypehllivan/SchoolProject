4c85a66ced8f6c15e73a98e4229137aa36ae202b
berkaypehllivan <berkaypehlivan75@gmail.com>
2024-01-03 22:34:47 +0300

    Level-1 Bitirildi

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DoorScript : MonoBehaviour
 {
+    public CanvasGroup InfoPanelCanvasGroup;
+    public TextMeshProUGUI InfoText;
     public GameObject player;
     public Sprite openDoorImage;
     public Sprite closeDoorImage;
     public float timeBeforeNextScene;
-    public bool playerIsAtTheDoor;
     private GameManager gameManager;
     private ScoreScript scoreScript;
     private AudioSource doorAudioSource;
@@ -17,6 +19,8 @@ public class DoorScript : MonoBehaviour
 
     public AudioClip openDoorSound;
     public AudioClip closeDoorSound;
+
+    private bool playerIsAtTheDoor;
 
     private void Start()
     {
@@ -28,27 +32,64 @@ public class DoorScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsAtTheDoor && gameManager.IsLevelCompleted())
+        if (Input.GetKeyDown(KeyCode.E) && playerIsAtTheDoor)
         {
-            StartCoroutine(OpenDoor());
-        }
-        else if (Input.GetKeyDown(KeyCode.E) && playerIsAtTheDoor && !gameManager.IsLevelCompleted())
-        {
-            Debug.Log("Bütün Düþmanlarý Öldürmen Gerekiyor! Kalan Düþman Sayýsý: " + scoreScript.remainingEnemies);
+            if (gameManager.IsLevelCompleted())
+            {
+                StartCoroutine(OpenDoor());
+            }
+            else
+            {
+                StartCoroutine(ShowInfoPanel());
+                Debug.Log("Bütün Düþmanlarý Öldürmen Gerekiyor! Kalan Düþman Sayýsý: " + scoreScript.remainingEnemies);
+            }
         }
     }
 
-    public IEnumerator OpenDoor()
+    private IEnumerator OpenDoor()
     {
         GetComponent<SpriteRenderer>().sprite = openDoorImage;
         yield return new WaitForSeconds(timeBeforeNextScene);
-        doorAudioSource.PlayOneShot(openDoorSound);
-        player.SetActive(false);
-        yield return new WaitForSeconds(timeBeforeNextScene);
         GetComponent<SpriteRenderer>().sprite = closeDoorImage;
+        doorAudioSource.PlayOneShot(openDoorSound);
+        yield return new WaitForSeconds(timeBeforeNextScene);
+        player.SetActive(false);
         fade.FadeIn();
         yield return new WaitForSeconds(timeBeforeNextScene);
         SceneManager.LoadScene("Level-2");
+    }
+
+    private IEnumerator ShowInfoPanel()
+    {
+        InfoText.text = "";
+        InfoPanelCanvasGroup.gameObject.SetActive(true);
+        InfoPanelCanvasGroup.alpha = 1f; // InfoPanel'i görünür yap
+        yield return TypeText("Bütün Düþmanlarý Öldürmen Gerekiyor! Kalan Düþman Sayýsý: " + scoreScript.remainingEnemies, 0.05f);
+        yield return new WaitForSeconds(1f);
+        yield return FadeOutCanvasGroup(InfoPanelCanvasGroup, 1.5f);
+        InfoPanelCanvasGroup.gameObject.SetActive(false);
+    }
+
+    private IEnumerator TypeText(string text, float typingSpeed)
+    {
+        foreach (char letter in text)
+        {
+            InfoText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private IEnumerator FadeOutCanvasGroup(CanvasGroup canvasGroup, float fadeDuration)
+    {
+        float timer = 0f;
+        while (timer < fadeDuration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, timer / fadeDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -67,4 +108,3 @@ public class DoorScript : MonoBehaviour
         }
     }
 }
-
